814ffa4caef79f156aa88b30a8fa7f2e2e53ef1d
robsonmrsp@gmail.com <robsonmrsp@gmail.com@da5606a0-c370-0573-d97a-a39dd41676bb>
2013-01-17 02:40:39 +0000

    ajustes

--- branches/storageposition-0.1-SNAPSHOT/src/main/java/br/com/mr/storageposition/client/StoragePosition.java
+++ branches/storageposition-0.1-SNAPSHOT/src/main/java/br/com/mr/storageposition/client/StoragePosition.java
@@ -1,13 +1,36 @@
 package br.com.mr.storageposition.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class StoragePosition implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
-		
+
 		Principal principal = new Principal();
+		MenuBar popupMenuBar = new MenuBar(true);
+		MenuItem alertItem = new MenuItem("Ultima posição..", true, new Command() {
+			@Override
+			public void execute() {
+
+			}
+		});
+		MenuItem imageItem = new MenuItem("Ultimas 10 posições...", true, new Command() {
+			@Override
+			public void execute() {
+				
+			}
+		});
+
+		alertItem.addStyleName("popup-item");
+		imageItem.addStyleName("popup-item");
+
+		popupMenuBar.addItem(alertItem);
+		popupMenuBar.addItem(imageItem);
+
 		RootPanel.get().add(principal);
 	}
 }
