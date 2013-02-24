41f63d50b5a81276a1f4897d4e0b0af0c4de3773
awdryn@gmail.com <awdryn@gmail.com@da5606a0-c370-0573-d97a-a39dd41676bb>
2013-02-24 23:49:39 +0000

    Prototipo funcinal do sistema gerenciador de paineis.

---
+++ src/test/java/com/far/eventmanagerpanels/domain/TipoEventoIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.far.eventmanagerpanels.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = TipoEvento.class)
+public class TipoEventoIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
